edc4dc181b9761a3a865cd7a9c547e8ddb0f0a07
Mikhail-Merchahskiy <112976682+Mikhail-Merchahskiy@users.noreply.github.com>
2022-09-22 20:30:43 +0300

    задание 1.1

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -1,9 +1,18 @@
+export const SORTASC = 'asc'
+export const SORTDESC = 'desc'
 /**
  * sortStrings - sorts array of string by two criteria "asc" or "desc"
  * @param {string[]} arr - the array of strings
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
 
+let sortType = {
+    [SORTASC]: 1,
+    [SORTDESC]: -1,
+};
+
+export function sortStrings(arr, param = SORTASC) {
+    let tempString = [...arr];
+  return tempString.sort( (string1, string2) =>  sortType[param] * string1.localeCompare(string2, 'ru-en', {caseFirst:'upper'}) );  
 }

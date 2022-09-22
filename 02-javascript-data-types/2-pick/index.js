351123a607f77600732f348339a468132e72ce12
Mikhail-Merchahskiy <112976682+Mikhail-Merchahskiy@users.noreply.github.com>
2022-09-22 21:19:30 +0300

    задача 2.2

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,9 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
+   let newObj = Object.fromEntries(
+    Object.entries(obj).filter(([key, value]) => fields.includes(key))
+   );
 
+   return newObj;
 };

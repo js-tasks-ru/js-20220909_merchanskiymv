ddb7544dcb3e8b49e7b0bcb4c896c75508c45273
Mikhail-Merchahskiy <112976682+Mikhail-Merchahskiy@users.noreply.github.com>
2022-10-05 14:04:03 +0300

    3.1

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,24 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  return function(obj = {}) 
+  { let fieldValue = obj;
+    let fieldsName = path.split('.');
+    // console.error(fieldsName);
+    for (let fieldName of fieldsName)
+    {
+      if (fieldValue === undefined) 
+      {
+        break;
+      }
 
+      fieldValue = fieldValue[fieldName];
+      //if typeof fieldValue === "object" && fieldValue !== null
+      //{
+      //    lobj =  fieldValue
+      //};
+    }
+
+    return fieldValue;
+  };
 }

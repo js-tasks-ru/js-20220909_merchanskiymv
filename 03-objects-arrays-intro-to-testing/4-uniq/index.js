3f15c0e403ee244afd8315c80e463d5b8c2abae9
Mikhail-Merchahskiy <112976682+Mikhail-Merchahskiy@users.noreply.github.com>
2022-10-07 14:23:41 +0300

    Задание 3.3

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -3,6 +3,15 @@
  * @param {*[]} arr - the array of primitive values
  * @returns {*[]} - the new array with uniq values
  */
-export function uniq(arr) {
 
+export function uniq(arr = []) {
+  const lSet = new Set(arr);
+  let lResultArray = [];
+
+  for (let key of lSet.keys())
+  {
+    lResultArray.push(key);
+  }
+
+  return lResultArray;
 }
